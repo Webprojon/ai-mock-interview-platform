37c08c11b5857fa957ae336d42cb36808fd43537
Webprojon <tokhirjonkhasanov@gmail.com>
2025-08-07 04:11:05 +0200

    feat: implemented interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "./DisplayTechIcons";
 
-export default function InterviewCard({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) {
+export default function InterviewCard({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) {
 	const feedback = null as Feedback | null;
 	const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
 	const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMM D, YYYY");
@@ -41,7 +41,7 @@ export default function InterviewCard({ interviewId, userId, role, type, techsta
 					<DisplayTechIcons techStack={techstack} />
 
 					<Button className="btn-primary">
-						<Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>{feedback ? "Check Feedback" : "View"}</Link>
+						<Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}>{feedback ? "Check Feedback" : "View Interview"}</Link>
 					</Button>
 				</div>
 			</div>
